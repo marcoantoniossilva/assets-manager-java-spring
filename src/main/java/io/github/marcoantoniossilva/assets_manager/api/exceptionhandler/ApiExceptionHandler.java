dfe633417859c38130e5033903b3d947e0c26635
Marco Silva <marcoantoniosilva.tec@gmail.com>
2022-03-30 19:14:33 -0300

    criado os exception handlers ApiExceptionHandler e Problem e as exceptions BusinessException e EntityNotFoundException

---
+++ src/main/java/io/github/marcoantoniossilva/assets_manager/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,77 @@
+package io.github.marcoantoniossilva.assets_manager.api.exceptionhandler;
+
+import io.github.marcoantoniossilva.assets_manager.domain.exception.BusinessException;
+import io.github.marcoantoniossilva.assets_manager.domain.exception.EntityNotFoundException;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+  private MessageSource messageSource;
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+    List<Problem.Field> fields = new ArrayList<>();
+
+    for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+      String name = ((FieldError) error).getField();
+      String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+
+      fields.add(new Problem.Field(name, message));
+    }
+
+    Problem problem = new Problem();
+    problem.setStatus(status.value());
+    problem.setDateTime(LocalDateTime.now());
+    problem.setTitle("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.");
+    problem.setFields(fields);
+
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @ExceptionHandler(BusinessException.class) /*Se uma BusinessException for lançada em qualquer lugar,
+	este método que será reponsável por tratá-la.*/
+  public ResponseEntity<Object> handleBusiness(BusinessException ex, WebRequest request) {
+
+    HttpStatus status = HttpStatus.BAD_REQUEST;
+
+    Problem problem = new Problem();
+    problem.setStatus(status.value());
+    problem.setDateTime(LocalDateTime.now());
+    problem.setTitle(ex.getMessage());
+
+    return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+  }
+
+  @ExceptionHandler(EntityNotFoundException.class) /*Se uma EntityNotFoundEncontradaException for lançada em qualquer lugar,
+	este método que será reponsável por tratá-la.*/
+  public ResponseEntity<Object> handleEntidadeNaoEncontrada(EntityNotFoundException ex, WebRequest request) {
+
+    HttpStatus status = HttpStatus.NOT_FOUND;
+
+    Problem problem = new Problem();
+    problem.setStatus(status.value());
+    problem.setDateTime(LocalDateTime.now());
+    problem.setTitle(ex.getMessage());
+
+    return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+  }
+
+}
