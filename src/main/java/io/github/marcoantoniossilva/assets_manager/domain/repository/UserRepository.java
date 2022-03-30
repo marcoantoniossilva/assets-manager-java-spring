e910d17f86dc7877d184d55bb70324bbfc3f5ec6
Marco Silva <marcoantoniosilva.tec@gmail.com>
2022-03-30 19:08:33 -0300

    criado metodo findByLogin no UserRepository

--- src/main/java/io/github/marcoantoniossilva/assets_manager/domain/repository/UserRepository.java
+++ src/main/java/io/github/marcoantoniossilva/assets_manager/domain/repository/UserRepository.java
@@ -3,7 +3,10 @@ package io.github.marcoantoniossilva.assets_manager.domain.repository;
 import io.github.marcoantoniossilva.assets_manager.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+  Optional<User> findByLogin(String login);
 
 }
