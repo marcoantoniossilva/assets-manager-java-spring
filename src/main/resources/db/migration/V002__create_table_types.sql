ecf36f193294132657c62bb3f99f8cb5d438144a
Marco Silva <marcoantoniosilva.tec@gmail.com>
2022-03-30 11:48:31 -0300

    adaptado projeto para usar migrations com Flyway.

---
+++ src/main/resources/db/migration/V002__create_table_types.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `types` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `description` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `depreciation` tinyint(3) unsigned DEFAULT NULL,
+  `depreciation_term` tinyint(3) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
