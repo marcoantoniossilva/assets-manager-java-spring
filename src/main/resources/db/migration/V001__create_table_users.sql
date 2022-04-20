8c02340052fdb06c39d8d549ad1508f5d55144c9
Marco Antonio Santos Silva <marcoantoniosilva.tec@gmail.com>
2022-04-20 11:14:10 -0300

    Removido caractere que sinaliza string nos scripts do flyway

--- src/main/resources/db/migration/V001__create_table_users.sql
+++ src/main/resources/db/migration/V001__create_table_users.sql
@@ -1,10 +1,10 @@
-CREATE TABLE 'users' (
-  'id' smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  'password' varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
-  'name' varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
-  'login' varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
-  'email' varchar(120) COLLATE utf8_unicode_ci NOT NULL,
-  'last_access' datetime DEFAULT NULL,
-  'register_in' datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY ('id')
+CREATE TABLE users (
+  id smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  password varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  name varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
+  login varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  email varchar(120) COLLATE utf8_unicode_ci NOT NULL,
+  last_access datetime DEFAULT NULL,
+  register_in datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
