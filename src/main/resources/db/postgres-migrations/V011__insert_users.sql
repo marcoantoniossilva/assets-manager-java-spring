699b4e02cca172043328a3abd6271304ccde9fa9
Marco Antonio Santos Silva <marcoantoniosilva.tec@gmail.com>
2022-04-20 12:45:23 -0300

    removido scrips de inserts de usuários

--- src/main/resources/db/postgres-migrations/V011__insert_users.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users VALUES
-    (1,'e10adc3949ba59abbe56e057f20f883e','Marco Antonio Santos Silva','marco','marco@gmail.com','2022-03-29 15:26:57','2020-12-23 22:57:56'),
-    (10,'44359f1cdac23622360704dd79086a25','Edvar Neves','Edvar ','lindaJIBOIAcontabil@gmail.com','2021-09-24 11:56:00','2020-12-24 08:22:46'),
-    (11,'f657674a5b96c7463917e31a68559fa4','Matheus','matheus','matheus@gmail.com','2022-02-14 09:22:17','2021-12-03 14:39:28');
